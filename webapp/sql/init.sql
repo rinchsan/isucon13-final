3d5625197361c51054b42fa79779ebaadb0f1077
toshiki-otaka <t.ohtaka@soda-inc.jp>
2023-11-25 12:08:51 +0900

    add index

--- webapp/sql/init.sql
+++ webapp/sql/init.sql
@@ -4,8 +4,31 @@ TRUNCATE TABLE reservation_slots;
 TRUNCATE TABLE livestream_viewers_history;
 TRUNCATE TABLE livecomment_reports;
 TRUNCATE TABLE ng_words;
-TRUNCATE TABLE reactions;
 TRUNCATE TABLE tags;
+TRUNCATE TABLE livestreams;
+TRUNCATE TABLE users;
+
+-- ライブ配信に対するライブコメント
+DROP TABLE IF EXISTS livecomments;
+CREATE TABLE `livecomments` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  `comment` VARCHAR(255) NOT NULL,
+  `tip` BIGINT NOT NULL DEFAULT 0,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+
+-- ライブ配信に対するリアクション
+DROP TABLE IF EXISTS reactions;
+CREATE TABLE `reactions` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_id` BIGINT NOT NULL,
+  `livestream_id` BIGINT NOT NULL,
+  -- :innocent:, :tada:, etc...
+  `emoji_name` VARCHAR(255) NOT NULL,
+  `created_at` BIGINT NOT NULL
+) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- ライブ配信とタグの中間テーブル
 DROP TABLE IF EXISTS livestream_tags;
@@ -15,9 +38,6 @@ CREATE TABLE `livestream_tags` (
   `tag_id` BIGINT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
-TRUNCATE TABLE livecomments;
-TRUNCATE TABLE livestreams;
-TRUNCATE TABLE users;
 
 ALTER TABLE `themes` auto_increment = 1;
 ALTER TABLE `icons` auto_increment = 1;
@@ -31,4 +51,7 @@ ALTER TABLE `tags` auto_increment = 1;
 ALTER TABLE `livecomments` auto_increment = 1;
 ALTER TABLE `livestreams` auto_increment = 1;
 ALTER TABLE `users` auto_increment = 1;
+
 alter table `livestream_tags` add index idx_01(livestream_id);
+alter table `reactions` add index idx_01(livestream_id);
+alter table `livecomments` add index idx_01(livestream_id);
