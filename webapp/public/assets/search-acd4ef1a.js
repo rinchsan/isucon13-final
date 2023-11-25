a773f3f3d07e815061a4f92519a035d008625ce7
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-11-25 10:33:12 +0900

    initial commit

---
+++ webapp/public/assets/search-acd4ef1a.js
@@ -0,0 +1,4 @@
+import{u as l,a as x,j as a,S as d,T as o,G as r,V as h,n as m}from"./index-55457d82.js";function j(){var e;const[n]=l(),s=n.get("q"),t=x({tag:s??""});return a.jsx(a.Fragment,{children:a.jsx(d,{sx:{mx:2,my:3},gap:3,children:a.jsxs(u,{children:[a.jsxs(o,{level:"h3",children:["検索結果 ",a.jsx("i",{children:s})]}),a.jsx(r,{container:!0,spacing:3,columns:4,flexGrow:1,sx:{padding:2},children:(e=t.data)==null?void 0:e.map((i,c)=>a.jsx(r,{xs:1,children:a.jsx(h,{liveSteram:i})},c))})]})})})}const u=m.div`
+  width: 1200px;
+  margin: 0 auto;
+`;export{j as default};
